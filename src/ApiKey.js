64360d991369d230f8e695273feb315af39b4d22
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-02-10 21:37:31 +0600

    api key set env

--- src/ApiKey.js
+++ src/ApiKey.js
@@ -1,12 +1,16 @@
-const key = "4271a1bb4e1bfa6d13784d54ab2cf5ad";
-
 const Api = {
-  popular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-  topRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-  upComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-  nowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+  popular: `https://api.themoviedb.org/3/movie/popular?api_key=${
+    import.meta.env.VITE_API_KEY
+  }&language=en-US&page=1`,
+  topRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${
+    import.meta.env.VITE_API_KEY
+  }&language=en-US&page=1`,
+  upComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${
+    import.meta.env.VITE_API_KEY
+  }&language=en-US&page=1`,
+  nowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${
+    import.meta.env.VITE_API_KEY
+  }&language=en-US&page=1`,
 };
 
 export default Api;
-
-// https://image.tmdb.org/t/p/w500  (/images)
